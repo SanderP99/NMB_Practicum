81b198b78d323943c83440941ffb2ba54256335c
Sander <prenen.sander@gmail.com>
2020-04-13 10:16:35 +0200

    Laatste aanpassingen

--- interpolerenChebyFout.m
+++ interpolerenChebyFout.m
@@ -1,29 +1,20 @@
 f1 = @(x) cos(x);
 f2 = @(x) 1./(25*x.^2+1);
-figure('Renderer', 'painters', 'Position', [10 10 900 600], 'PaperPositionMode', 'auto');
+figure('Renderer', 'painters', 'Position', [10 10 1400 600], 'PaperPositionMode', 'auto');
 set(gca, 'Units', 'normalized','FontUnits','points','FontWeight','normal','FontSize',14,'FontName','Times')
-
+hold on
+tiledlayout(2,3)
 %fplot(f1)
 
 % Punten om te evalueren
-x = zeros(1,300);
-for i = 1:1:200
-    x(i) = -1 + 1/200 + 2/200*(i-1);
-end
+x = linspace(-1,1,300);
 
-maxima = zeros(1,200);
-n_maxima = zeros(1,200);
-
-for n = 1:1:200
-    n_maxima(n) = n;
+for n = [15,100, 200]
+    nexttile
     N = n;
     % Equidistante punten
-    x_equi = zeros(1,N);
-    for i = 1:1:N
-        x_equi(i) = -1 + (i-1) .* 2/(N-1);
-    end
-    x_equi(N) = 1;
-
+    x_equi = linspace(-1,1,N);
+    
     % Nulpunten
     x_nul = zeros(1,N);
     for i = 1:1:N
@@ -32,22 +23,63 @@ for n = 1:1:200
 
     [c, kappa] = interpolate(x_equi, f2);
     interpolant = evalCheb(c', x);
-    maxFout = 0;
-    for i = 1:1:200
-        afstand = abs(interpolant(i) - f2(x(i)));
-        if afstand > maxima(n)
-            maxima(n) = afstand;
-        end
+    
+    difference = zeros(1,300);
+    for i = 1:300
+        difference(i) = interpolant(i) - f2(x(i));
     end
-        
+    hold on
+    semilogy(x,difference);
+    if N < 20
+        semilogy(x_equi, zeros(size(x_equi)), 'o','MarkerSize',10)
+    end
+    hold off
+    hline = refline(0, 0);
+    hline.Color = 'k';
+    ylabel('Residu', 'interpreter', 'latex')
+    set(gca, 'Units', 'normalized','FontUnits','points','FontWeight','normal','FontSize',14,'FontName','Times')
+legend({strcat('n=',' ',  num2str(N))}, 'interpreter', 'latex', 'location', 'north', 'NumColumns', 1, 'FontSize', 20,'FontWeight', 'normal', 'FontName', 'Times');
+
+    
+end
+
+for n = [15,100,200]
+    nexttile
+    N = n;
+    % Equidistante punten
+    x_equi = linspace(-1,1,N);
+    
+    % Nulpunten
+    x_nul = zeros(1,N);
+    for i = 1:1:N
+        x_nul(i) = cos(pi*(2*i-1)/(2*N));
+    end
+
+    [c, kappa] = interpolate(x_nul, f2);
+    interpolant = evalCheb(c', x);
+    
+    difference = zeros(1,300);
+    for i = 1:300
+        difference(i) = interpolant(i) - f2(x(i));
+    end
+    hold on
+    semilogy(x,difference);
+    if N < 20
+        semilogy(x_nul, zeros(size(x_nul)), 'o','MarkerSize',10)
+    end
+    hold off
+    hline = refline(0, 0);
+    hline.Color = 'k';
+    ylabel('Residu', 'interpreter', 'latex')
+    set(gca, 'Units', 'normalized','FontUnits','points','FontWeight','normal','FontSize',14,'FontName','Times')
+    legend({strcat('n=',' ',  num2str(N))}, 'interpreter', 'latex', 'location', 'north', 'NumColumns', 1, 'FontSize', 20,'FontWeight', 'normal', 'FontName', 'Times');
+
     
 end
 %legend({'Fout'}, 'interpreter', 'latex', 'location', 'south', 'NumColumns', 1, 'FontSize', 20,'FontWeight', 'normal', 'FontName', 'Times');
-semilogy(n_maxima, maxima)
-set(gca, 'Units', 'normalized','FontUnits','points','FontWeight','normal','FontSize',14,'FontName','Times')
-%legend({'Maximaal residu'}, 'interpreter', 'latex', 'location', 'northeast', 'NumColumns', 1, 'FontSize', 20,'FontWeight', 'normal', 'FontName', 'Times');
-xlabel('$n$', 'interpreter', 'latex')
-ylabel('Maximaal residu', 'interpreter', 'latex')
-%axis([-1 1 0 1]);
-
-print  ('-r500', '-depsc2', 'runge_equi_fout.eps')
+%set(gca, 'Units', 'normalized','FontUnits','points','FontWeight','normal','FontSize',14,'FontName','Times')
+%legend({'100'}, 'interpreter', 'latex', 'location', 'southeast', 'NumColumns', 1, 'FontSize', 20,'FontWeight', 'normal', 'FontName', 'Times');
+%ylabel('Fout $g - f$', 'interpreter', 'latex')
+%axis([-1 1 0 8]);
+hold off
+print  ('-r500', '-depsc2', 'runge_heleFout.eps')
